9a06c5014c7f6d774a5685ba18aecce316873270
AkhmadievALbert <a.akhmadiev@tinkoff.ru>
2021-10-25 03:13:08 +0300

    [init] Project

---
+++ Generic/Generic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Generic
+//
+//  Created by a.akhmadiev on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
